78c1d499c1f59a12c468218e8ecc41c8146219a6
jeffmarti <46584819+jeffmarti@users.noreply.github.com>
2026-03-14 16:58:08 -0700

    Update app.R

--- app.R
+++ app.R
@@ -60,11 +60,11 @@ ui <- fluidPage(
           "months when irrigation demand is highest."
         ),
         tags$p(
-          "This dashboard combines daily reservoir storage volume numbers from USBR Hydromet with",
+          "This dashboard combines daily reservoir storage from USBR Hydromet with",
           "basin snowpack estimates derived from SNODAS data provided by the National",
           "Operational Hydrologic Remote Sensing Center (NOHRSC) and Climate Engine.",
-          "Together these values represent total constructed and above-ground water storage.",
-          "Note that this estimate does not include groundwater or soil moisture",
+          "Together these numbers represent total constructed and above-ground water storage.",
+          "This estimate does not include groundwater or soil moisture",
           "recharged by rain and snowmelt infiltration."
         ),
         tags$p(
